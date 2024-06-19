caf5e1a876c0ebe2788625efad6303310b351d4c
Dancingaroundthelies <its.azizaah@gmail.com>
2024-06-19 21:03:13 +0700

    rapihin detail artikel

--- src/pages/DetailArtikel.jsx
+++ src/pages/DetailArtikel.jsx
@@ -40,17 +40,19 @@ const DetailArticle = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="max-w-3xl mx-auto bg-white shadow-md rounded-md p-6 mb-8">
-        <h1 className="text-2xl font-bold mb-4">{articleDetail.title}</h1>
-        <div className="flex flex-col items-center mb-4">
-          <img src={articleDetail.image} className="w-full object-cover rounded-md mr-4" alt="artikel 1" />
-          <div>
-            <p className="text-gray-600">{new Date(articleDetail.date).toLocaleDateString()}</p>
-            <p className="text-gray-600">{articleDetail.author}</p>
+    <div className="container mx-auto px-4 py-8 bg-DS-beige-100 shadow-lg rounded-md">
+      <div className="contentt mx-auto flex flex-col max-w-[1200px]">
+        <div className="flex flex-col items-center gap-9 p-6 mb-8">
+          <h1 className="text-5xl font-bold mb-4">{articleDetail.title}</h1>
+          <div className="flex flex-col items-start gap-5 xl:w-full">
+            <img src={articleDetail.image} className="w-[935px] max-h-[490px] object-cover rounded-lg md:max-h-48 xl:w-full" alt="artikel 1" />
+            <div className="flex items-start gap-5 font-medium text-lg md:text-sm">
+              <p className="text-gray-600">{new Date(articleDetail.date).toLocaleDateString()}</p>
+              <p className="text-gray-600">{articleDetail.author}</p>
+            </div>
           </div>
+          <div className="text-gray-700 text-justify text-xl font-normal">{articleDetail.content}</div>
         </div>
-        <div className="text-gray-700">{articleDetail.content}</div>
       </div>
     </div>
   );
