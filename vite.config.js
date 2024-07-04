0899cf62fbe1bba37fad812c094083219c0bbaba
Sarah Nurhasna <sarah2501.snk@gmail.com>
2024-07-04 15:48:57 +0700

    Fix bug PWA

--- vite.config.js
+++ vite.config.js
@@ -5,33 +5,55 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     VitePWA({
       manifest: {
         icons: [
           {
             src: "/image/hands-logo.svg",
             sizes: "any",
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          }
-        ]
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icon/moon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "/icon/sun.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+        ],
       },
       workbox: {
-        runtimeCaching: [{
-          urlPattern: ({ url }) => {
-            return url.pathname.startsWith("https://dialog-sunyi-be.vercel.app/api");
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.href.startsWith("https://dialog-sunyi-be.vercel.app/api/"),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
           },
-          handler: "CacheFirst",
-          options: {
-            cacheName: "api-cache",
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }]
-      }
-    })
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith("/image/"),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              }
+            },
+          },
+        ],
+      },
+    }),
   ],
   css: {
     postcss: "./postcss.config.js",
